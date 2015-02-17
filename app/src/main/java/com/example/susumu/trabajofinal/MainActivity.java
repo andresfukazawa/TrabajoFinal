af0d2b618d75e6b984d082493b4811d0707da21d
andresfukazawa <fukazawa@gmail.com>
2015-02-17 00:34:32 -0500

    Funcionalidad basica de la pagina de logueo esta practicamente completa.

--- app/src/main/java/com/example/susumu/trabajofinal/MainActivity.java
+++ app/src/main/java/com/example/susumu/trabajofinal/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.susumu.trabajofinal;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.susumu.trabajofinal.dao.DataBaseHelper;
+import com.example.susumu.trabajofinal.dao.UsuarioDAO;
+import com.example.susumu.trabajofinal.entities.Usuario;
+
+import java.security.MessageDigest;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -17,11 +22,19 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         etNombre     = (EditText)findViewById(R.id.etLINombre);
         etContrasena = (EditText)findViewById(R.id.etLIContrasena);
         btIngresar   = (Button)findViewById(R.id.btLIIngresar);
+
+        try {
+            DataBaseHelper dataBaseHelper = new DataBaseHelper(MainActivity.this);
+            dataBaseHelper.createDataBase();
+            dataBaseHelper.openDataBase();
+        } catch (Exception ex) {
+            Toast.makeText(MainActivity.this, "No se pudo copiar la BD", Toast.LENGTH_SHORT).show();
+        }
 
         btIngresar.setOnClickListener(btIngresarOnClick);
     }
@@ -31,8 +44,38 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public void onClick(View v) {
 
+            Usuario usuario = new Usuario();
+            String  msg     = "";
+
+            try {
+                MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+                messageDigest.update(etContrasena.getText().toString().getBytes());
+                byte byteData[] = messageDigest.digest();
+
+                StringBuffer sb = new StringBuffer();
+                for (int i = 0; i < byteData.length; i++) {
+                    sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+                }
+
+                String pwdhash = sb.toString();
+
+                UsuarioDAO usuarioDAO = new UsuarioDAO();
+                usuario = usuarioDAO.getUsuarioByUserId(etNombre.getText().toString(), pwdhash);
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+            if (usuario == null) {
+                msg = "Usuario no encontrado";
+            } else {
+                msg = usuario.getNombre() + " " + usuario.getApellido();
+            }
+
+            Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+
         }
-    }
+    };
 
 
 }
--- app/src/main/java/com/example/susumu/trabajofinal/dao/UsuarioDAO.java
+++ app/src/main/java/com/example/susumu/trabajofinal/dao/UsuarioDAO.java
@@ -1,7 +1,10 @@
 package com.example.susumu.trabajofinal.dao;
 
 import android.database.Cursor;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.example.susumu.trabajofinal.MainActivity;
 import com.example.susumu.trabajofinal.entities.Usuario;
 
 /**
@@ -16,15 +19,19 @@ public class UsuarioDAO {
 
         try{
             cursor = DataBaseHelper.myDataBase.query("usuarios", null, "userId=? and password=?", new String[]{userId, password}, null, null, null);
+            Log.d("userId", userId);
 
             if(cursor.moveToFirst()) {
+                Log.d("hola ! ", userId);
                 do {
+                    Log.d("hello ! ", userId);
                     usuario = new Usuario();
                     usuario.setUsuario(userId);
-                    usuario.setNombre(cursor.isNull(cursor.getColumnIndex("Nombre"))?"":cursor.getString(cursor.getColumnIndex("Nombre")));
-                    usuario.setApellido(cursor.isNull(cursor.getColumnIndex("Apellido"))?"":cursor.getString(cursor.getColumnIndex("Apellido")));
+                    usuario.setNombre(cursor.isNull(cursor.getColumnIndex("nombre")) ? "" : cursor.getString(cursor.getColumnIndex("nombre")));
+                    usuario.setApellido(cursor.isNull(cursor.getColumnIndex("apellido")) ? "" : cursor.getString(cursor.getColumnIndex("apellido")));
 
                 } while (cursor.moveToNext());
+                Log.d("apellido", usuario.getApellido());
             }
 
         } catch (Exception ex) {
