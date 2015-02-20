86c1c448922159fb71847c07f3f99aff8bdea813
andresfukazawa <fukazawa@gmail.com>
2015-02-19 22:39:08 -0500

    Se anadio el menu principal con dos botones:  Tomar pedidos y Pendientes

--- app/src/main/java/com/example/susumu/trabajofinal/SeleccionarMesaActivity.java
+++ app/src/main/java/com/example/susumu/trabajofinal/SeleccionarMesaActivity.java
@@ -31,14 +31,6 @@ public class SeleccionarMesaActivity extends ActionBarActivity {
         spMesa        = (Spinner)findViewById(R.id.spMesas);
         btTomarPedido = (Button)findViewById(R.id.btIOTomarPedido);
 
-//        try {
-//            DataBaseHelper dataBaseHelper = new DataBaseHelper(SeleccionarMesaActivity.this);
-//            dataBaseHelper.createDataBase();
-//            dataBaseHelper.openDataBase();
-//        } catch (Exception ex) {
-//            Toast.makeText(SeleccionarMesaActivity.this, "No se pudo copiar la BD", Toast.LENGTH_SHORT).show();
-//        }
-
         btTomarPedido.setOnClickListener(btTomarPedidoOnClick);
         populateSpinner();
 
@@ -58,30 +50,21 @@ public class SeleccionarMesaActivity extends ActionBarActivity {
         Cursor cursor = null;
 
         try{
-            Toast.makeText(SeleccionarMesaActivity.this, "point 0", Toast.LENGTH_SHORT).show();
             cursor = DataBaseHelper.myDataBase.query("mesas", new String[]{"idx"}, null, null, null, null, "idx");
-            Toast.makeText(SeleccionarMesaActivity.this, "point 1", Toast.LENGTH_SHORT).show();
             if(cursor.moveToFirst()) {
-                Toast.makeText(SeleccionarMesaActivity.this, "point 2", Toast.LENGTH_SHORT).show();
                 do {
-                    Toast.makeText(SeleccionarMesaActivity.this, "point 3", Toast.LENGTH_SHORT).show();
-                    list.add("Mesa " + Integer.valueOf(cursor.getInt(cursor.getColumnIndex("idx"))));
-                    Toast.makeText(SeleccionarMesaActivity.this, Integer.valueOf(cursor.getInt(cursor.getColumnIndex("idx"))), Toast.LENGTH_SHORT).show();
+                    list.add("Mesa " + cursor.getInt(cursor.getColumnIndex("idx"))+"");
                 } while (cursor.moveToNext());
-                Toast.makeText(SeleccionarMesaActivity.this, "point 4", Toast.LENGTH_SHORT).show();
             }
-            Toast.makeText(SeleccionarMesaActivity.this, "point 5", Toast.LENGTH_SHORT).show();
-
         } catch (Exception ex) {
             ex.printStackTrace();
-            Toast.makeText(SeleccionarMesaActivity.this, ex.toString(), Toast.LENGTH_LONG).show();
-            Log.d("Error", ex.toString());
+//            Toast.makeText(SeleccionarMesaActivity.this, ex.toString(), Toast.LENGTH_LONG).show();
+//            Log.d("Error", ex.toString());
         } finally {
             if (cursor != null)
                 cursor.close();
         }
 
-        Toast.makeText(SeleccionarMesaActivity.this, "Finish", Toast.LENGTH_SHORT).show();
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spMesa.setAdapter(dataAdapter);
